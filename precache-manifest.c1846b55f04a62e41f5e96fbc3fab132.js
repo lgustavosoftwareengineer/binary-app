9d8ca135e3134e797c9734b3ee33d2b92e4652e3
Luiz Gustavo <gugamatias7@gmail.com>
2020-07-04 15:05:16 -0300

    Updates

---
+++ precache-manifest.c1846b55f04a62e41f5e96fbc3fab132.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e595dec35152b737c8eba24dcc2c7354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd2a8228e2bcfe2b2953",
+    "url": "/static/css/main.39502d36.chunk.css"
+  },
+  {
+    "revision": "e35500064020ddd1a7ee",
+    "url": "/static/js/2.f89fefa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f89fefa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2a8228e2bcfe2b2953",
+    "url": "/static/js/main.8e2a83e4.chunk.js"
+  },
+  {
+    "revision": "3f59163199d2a96c7300",
+    "url": "/static/js/runtime-main.36e3a51c.js"
+  },
+  {
+    "revision": "aafc4100c3df1424d0a5be7a7188ba90",
+    "url": "/static/media/logo.aafc4100.png"
+  }
+]);
